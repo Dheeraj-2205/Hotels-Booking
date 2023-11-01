4fc7447d65e3e34c8a86301a0caa7069fc9152ff
Dheeraj-2205 <djsays124@gmail.com>
2023-11-01 23:09:23 +0530

    header and 2 vanner is complted

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -27,8 +27,33 @@ const Banner = () => {
           className=" h-16 outline-none px-3 text-lg
           border-r-2 border-gray-400 col-span-1"
         />
-        <button type="submit" className="h-16 px-3 py-2 cols-span-1 bg-red-600 hover:cursor-pointer hover:bg-red-400">
-            Search
+        <button
+          type="submit"
+          className="h-16 px-3 py-2 cols-span-1 bg-red-600 hover:cursor-pointer hover:bg-red-400 text-lg text-white"
+        >
+          Search
+        </button>
+      </div>
+      <div className="flex mx-20 my-5">
+        <button
+          type="submit"
+          className="h-15 px-3 py-2 hover:cursor-pointer
+          border-2 border-white text-white 
+          mr-5 hover:bg-slate-100 hover:text-black
+          rounded-xl
+          "
+        >
+          Search
+        </button>
+        <button
+          type="submit"
+          className="h-15 px-3 py-2 hover:cursor-pointer
+          border-2 border-white text-white 
+          mr-5 hover:bg-slate-100 hover:text-black
+          rounded-xl
+          "
+        >
+          Domestic Hotels
         </button>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import Header from '../components/Header'
-import PlaceToVisit from '@/components/PlaceToVisit'
-import Banner from '@/components/Banner'
+import React from "react";
+import Header from "../components/Header";
+import PlaceToVisit from "@/components/PlaceToVisit";
+import Banner from "@/components/Banner";
+import Image from "next/image";
 const Index = () => {
   return (
     <div>
-      <Header/>
-      <PlaceToVisit/>
-      <Banner/>
+      <Header />
+      <PlaceToVisit />
+      <Banner />
+    
+      <div className="max-10 my-14">
+        <Image
+          src={"/luxury.jpg"}
+          alt="Banner1"
+          width={200}
+          height={200}
+          className="h-80 w-full"
+        />
+      </div>
+      <div className="mb-14">
+        <Image
+          src={""}
+          alt="Banner2"
+          width={200}
+          height={200}
+          className="h-40 w-full"
+        />
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
